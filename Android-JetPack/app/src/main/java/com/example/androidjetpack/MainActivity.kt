ea24068f08d5bf4c1fe456737d5a87d67c05ba76
Kuuga2000 <ryuki.servaiv@gmail.com>
2023-02-01 08:28:39 +0700

    Rows and Column

--- Android-JetPack/app/src/main/java/com/example/androidjetpack/MainActivity.kt
+++ Android-JetPack/app/src/main/java/com/example/androidjetpack/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.androidjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidjetpack.ui.theme.AndroidJetPackTheme
+import com.example.androidjetpack.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column() {
+                        Greeting("Android")
+                        CustomText(name = "Grim Stroke")
+                    }
                 }
             }
         }
@@ -34,10 +42,40 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun CustomText(name: String) {
+    Text(
+        text = name,
+        style = Typography.h1
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidJetPackTheme {
-        Greeting("Android")
+        Column(
+            modifier= Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Surface(
+                modifier = Modifier.width(200.dp).weight(1f),
+                color = MaterialTheme.colors.secondary
+            ) {
+
+            }
+
+            Surface(
+                modifier = Modifier.width(200.dp).weight(3f),
+                color = MaterialTheme.colors.primary
+            ) {
+
+            }
+
+
+
+        }
+
     }
 }
