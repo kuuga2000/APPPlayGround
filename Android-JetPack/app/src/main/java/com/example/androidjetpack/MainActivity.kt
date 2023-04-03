4e0971572ef9daed59fc3a2b33abf4c6f5259096
Kuuga2000 <--edit>
2023-04-03 22:53:21 +0700

    dp

--- Android-JetPack/app/src/main/java/com/example/androidjetpack/MainActivity.kt
+++ Android-JetPack/app/src/main/java/com/example/androidjetpack/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Column(
-                modifier = Modifier.fillMaxSize(0.5f)
+                modifier = Modifier.width(20.dp).height(500.dp)
                     .background(Color.Yellow),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
