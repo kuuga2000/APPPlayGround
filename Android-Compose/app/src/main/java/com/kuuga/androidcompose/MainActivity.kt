5e1f439c1db1266ddfc189a72173bb3c5277a7d1
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-04-07 20:42:49 +0700

    Text Color, FontSize, Column,

--- Android-Compose/app/src/main/java/com/kuuga/androidcompose/MainActivity.kt
+++ Android-Compose/app/src/main/java/com/kuuga/androidcompose/MainActivity.kt
@@ -3,8 +3,14 @@ package com.kuuga.androidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text;
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.kuuga.androidcompose.ui.theme.AndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +18,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidComposeTheme {
-                Text("Hello World", color = Color.Red)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth().background(Color.Red)
+                ) {
+                    Text("Hello World",
+                        color = Color(0xFFF0000FF),
+                        fontSize = 16.sp
+                    )
+                    Text("Hello World",
+                        color = Color(0xFFF0000FF),
+                        fontSize = 30.sp
+                    )
+                }
             }
         }
     }
