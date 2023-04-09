25e34e3d1c4358566e8e49216107f1a8890e3df0
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-04-09 17:41:20 +0700

    Lemonade

---
+++ Compose-Lemonade/app/src/main/java/com/kuuga/composelemonade/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.kuuga.composelemonade
+
+import android.os.Bundle
+import android.provider.CalendarContract
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.kuuga.composelemonade.ui.theme.ComposeLemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLemonadeTheme {
+                ButtonImage()
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonImage() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        var lemon by remember { mutableStateOf(1) }
+        var imageResource = when (lemon) {
+            1->R.drawable.lemon_tree
+            2->R.drawable.lemon_squeeze
+            3->R.drawable.lemon_drink
+            4->R.drawable.lemon_restart
+            else->R.drawable.lemon_restart
+        }
+
+        Column(
+            modifier = Modifier.background(Color(0xFF006994)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(lemon.toString())
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = "Lemon Tree",
+                modifier = Modifier.clickable {
+                    lemon = (1..4).random()
+                }
+            )
+        }
+    }
+}
