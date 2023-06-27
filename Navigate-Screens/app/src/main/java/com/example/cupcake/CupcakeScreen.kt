ea76de5af893dbf2c030e47150074fd3fa76a847
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-06-27 12:11:46 +0700

    documentation

--- Navigate-Screens/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Navigate-Screens/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -213,7 +213,50 @@ private fun shareOrder(
 
 
 /**
- * note*
+ * In modern Android development,
+ * multiscreen apps are created using the Jetpack Navigation component.
+ * The Navigation Compose component allows you to easily build multi screen apps in Compose using a declarative approach,
+ * just like building user interfaces. This codelab introduces the essentials of the Navigation Compose component,
+ * how to make the AppBar responsive,
+ * and how to send data from your app to another app using intents—all while demonstrating best practices in an increasingly complex app.
+ *
+ * What you'll learn ->
+ * Create a NavHost composable to define routes and screens in your app.
+ * Navigate between screens using a NavHostController.
+ * Manipulate the backstack to navigate to previous screens.
+ * Use intents to share data with another app.
+ * Customize the AppBar, including the title and back button.
+ *
+ *
+ * What you'll build ->
+ * You'll implement navigation in a multiscreen app.
+ *
+ * What you'll need ->
+ * The latest version of Android Studio
+ * An internet connection to download the starter code
+ *
+ * Define routes and create a NavHostController ->
+ * Parts of the Navigation Component
+ * The Navigation component has three main parts:
+ *
+ * NavController: Responsible for navigating between destinations—that is, the screens in your app.
+ * NavGraph: Maps composable destinations to navigate to.
+ * NavHost: Composable acting as a container for displaying the current destination of the NavGraph.
+ * In this codelab, you'll focus on the NavController and the NavHost. Within the NavHost,
+ * you'll define the destinations for the Cupcake app's NavGraph.
+ *
+ * Note*
+ * NavHostController is a subclass of the NavController class that provides additional functionality for use with a NavHost composable.
+ *
+ * There are two notable parameters.
+ * navController: An instance of the NavHostController class. You can use this object to navigate between screens,
+ * for example, by calling the navigate() method to navigate to another destination.
+ * You can obtain the NavHostController by calling rememberNavController() from a composable function.
+ * startDestination: A string route defining the destination shown by default when the app first displays the NavHost.
+ * In the case of the Cupcake app, this should be the Start route.
+ * Like other composables, NavHost also takes a modifier parameter.
+ *
+ * Note*
  * You should now be able to navigate through each screen of your app.
  * Notice that by calling navigate(), not only does the screen change,
  * but it's actually placed on top of the backstack.
@@ -224,4 +267,11 @@ private fun shareOrder(
  * Doc ->
  * Send simple data to other apps
  * https://developer.android.com/training/sharing/send#kotlin
+ *
+ * Summary
+ * Learn More ->
+ *  - Navigating with Compose https://developer.android.com/jetpack/compose/navigation
+ *  - Navigation principles https://developer.android.com/guide/navigation/navigation-principles
+ *  - Jetpack Compose Navigation https://developer.android.com/codelabs/jetpack-compose-navigation#0
+ *  - Types of Navigation https://material.io/design/navigation/understanding-navigation.html
  */
