807b84558dbcac5520b402c8aab6b490dc960b05
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-06 01:11:59 +0700

    Learn More

--- Retrofit/app/src/main/java/com/example/marsphotos/MainActivity.kt
+++ Retrofit/app/src/main/java/com/example/marsphotos/MainActivity.kt
@@ -44,3 +44,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+// Learn More : https://developer.android.com/codelabs/basic-android-kotlin-compose-getting-data-internet?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-5-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-getting-data-internet#10
