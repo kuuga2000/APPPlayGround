61aaa23438770c96324f143e0ffe7ece58f7f82b
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-06 17:09:30 +0700

    Dependency injection remove unsed code

--- Data-UI-Layer/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ Data-UI-Layer/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -17,25 +17,10 @@
 package com.example.marsphotos.network
 
 import com.example.marsphotos.model.MarsPhoto
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 import retrofit2.http.GET
 
-private const val BASE_URL =
-    "https://android-kotlin-fun-mars-server.appspot.com"
-
 /**
- * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
- */
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-    .baseUrl(BASE_URL)
-    .build()
-
-/**
- * Retrofit service object for creating api calls
+ * A public interface that exposes the [getPhotos] method
  */
 interface MarsApiService {
     /**
