558e85077b1517bafca7e993f6d3b839a4ac2c4d
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-06 18:44:04 +0700

    Add Coil dependency, Display the Image URL

--- Internet-Load-Image/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ Internet-Load-Image/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -60,10 +60,8 @@ class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : Vi
         viewModelScope.launch {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-                val listResult = marsPhotosRepository.getMarsPhotos()
-                MarsUiState.Success(
-                    "Success: ${listResult.size} Mars photos retrieved"
-                )
+                val result = marsPhotosRepository.getMarsPhotos()[0]
+                MarsUiState.Success("First Mars image URL: ${result.imgSrc}")
             } catch (e: IOException) {
                 MarsUiState.Error
             } catch (e: HttpException) {
